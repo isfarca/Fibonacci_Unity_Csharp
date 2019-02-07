3837d28c7f9e999a7aa904489038b9f7b16cd831
Fethi Isfarca <fisfarca@googlemail.com>
2019-02-07 20:27:32 +0100

    Adding project.

---
+++ Fibonacci/Assets/Fibonacci.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Fibonacci : MonoBehaviour
+{
+	// Use this for initialization
+	void Start()
+    {
+        // Declare variables
+        int n = 8;
+
+        // Output the fibonacci-values in the short version.
+        Debug.Log("---------- Short version ----------");
+        Debug.LogFormat("' {0}. ' ' {1} '", n, MyMathFunctions.Fibonacci(n));
+
+        // Output the fibonacci-values in the long version.
+        Debug.Log("---------- Long version ----------");
+        Debug.LogFormat("' {0}. ' ' {1} '", n, MyMathFunctions.FibonacciProcedural(n));
+    }
+}
---
+++ Fibonacci/Assets/MyMathFunctions.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class MyMathFunctions
+{
+    /// <summary>
+    /// Algorithm from fibonacci (short version).
+    /// </summary>
+    /// <param name="n">Number of passes.</param>
+    /// <returns>End result from fibonacci.</returns>
+    public static int Fibonacci(int n)
+    {
+        if (n == 0 || n == 1)
+            return 1;
+
+        return Fibonacci(n - 1) + Fibonacci(n - 2);
+    }
+
+    /// <summary>
+    /// Algorithm from fibonacci (long version).
+    /// </summary>
+    /// <param name="n">Number of passes.</param>
+    /// <returns>End result from fibonacci.</returns>
+    public static int FibonacciProcedural(int n)
+    {
+        // Declare variables
+        int a = 1;
+        int b = 1;
+        int temp;
+
+        for (int i = 0; i < n; i++)
+        {
+            temp = a;
+            a = b;
+            b = b + temp;
+        }
+
+        return a;
+    }
+}
